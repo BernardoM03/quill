06b69da775bbd0963b442fa38899f6347a6acc7b
bernardo mendes <107783717+BernardoM03@users.noreply.github.com>
2026-05-26 14:20:15 -0700

    Added a rules route/page to display the core rules

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
 import './index.css'
-import App from './App.tsx'
+
+//Routes and React Elements
+import Rules from './routes/Rules'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,3 +13,18 @@ createRoot(document.getElementById('root')!).render(
     </BrowserRouter>
   </StrictMode>,
 )
+
+function App() {
+  return (
+    <>
+      <h1>Quill</h1>
+      <nav>
+        <Link to="/rules">Rules</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/rules" element={<Rules />} />
+      </Routes>
+    </>
+  )
+}
